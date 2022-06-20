cd61e3336be4789bbf04d1fdce270e22fb5fbb37
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-06-20 10:17:52 -1100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """ prints x elements of a list.
-    Args:
-        x (int): The number of element to print.
-        my_list (list): The list of element to print
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
-    real_number_of_element_return = 0
-
-    for i in range(0, x):
+    cmpt = 0
+    for index in range(x):
         try:
-            print(f"{my_list[i]}", end="")
-            real_number_of_element_return += 1
-        except IndexError:
+            print(my_list[index], end='')
+            cmpt += 1
+        except Exception as error:
             break
-    print("")
-    return real_number_of_element_return
+    print('')
+    return cmpt
