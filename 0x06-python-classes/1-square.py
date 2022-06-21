2d4d58cd0300926c89e12a55d016223c6445c64e
joepiano <joepiano306@gmail.com>
2022-06-21 09:11:45 -0700

    103-magic_class.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Structure of square."""
+
+    def __init__(self, size):
+        """Constructor of square.
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
