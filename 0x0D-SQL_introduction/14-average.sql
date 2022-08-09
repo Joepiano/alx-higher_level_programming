27bcb338f4d48afa01fdb94f06a8502f7084229c
joepiano <joepiano306@gmail.com>
2022-08-09 16:06:49 -0700

    sql intro

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,9 @@
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
