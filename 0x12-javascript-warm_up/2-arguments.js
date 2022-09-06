8289583d102717b39601f1cdbb307b35a4e0ecaf
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-09-06 11:19:36 -1100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
+
 #!/usr/bin/node
-const argv = process.argv
-if (argv.length < 3){
-	console.log('No argument');
-}else if(argv.length === 3 ){
-		console.log('Argument found');
-	{ else {
-		console.log(Arugumet found);
-	}
+const argv = process.argv;
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
