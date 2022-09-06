8e4dffe61c00d3cc7953f1a2bf1b90a7c4faa829
Joepiano <99508912+Joepiano@users.noreply.github.com>
2022-09-06 12:11:48 -1100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 
 class Rectangle {
-	  constru.width = w;
-	      this.height = h;
-	    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-  prctor (w, h) {
-	      if (w > 0 && h > 0) {
-		            thisint () {
-				        for (let i = 0; i < this.height; i++) {
-						      console.log('X'.repeat(this.width));
-						    }
-				      }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 
-		        rotate () {
-				    const tmp = this.height;
-				    this.height = this.width;
-				    this.width = tmp;
-				  }
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
 
-		        double () {
-				    this.height *= 2;
-				    this.width *= 2;
-				  }
-	      }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
 
-	  module.exports = Rectangle;
+module.exports = Rectangle;
